228181e0414c698cf08c9319059dffb9e21f3b61
hieuducnguyen.it@gmail.com <hieuducnguyen.it@gmail.com>
2021-08-22 11:11:17 +0700

    Refactor code: create router, app/controllers folder

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 const path = require('path');
 const express = require('express');
-const morgan = require('morgan');
+const morgan = require('morgan'); //lang nghe request tu client
 const handlebars = require('express-handlebars');
-const app = express();
-const port = 3000
+const app = express(); //khoi tao instance
+const port = 3000 //set port
+
+const route = require('./routes');
 
 // join to public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// use middleware (thanh phan trung gian giua client va server)
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', handlebars({
@@ -18,14 +26,8 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs'); // use handlebars
 app.set('views', path.join(__dirname, 'resources/views'));
 
-// route
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
